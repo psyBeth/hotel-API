2e7c2c822de4561f8b0422011172b5bd29b3acd2
Betul <betulkoru06@gmail.com>
2024-05-11 16:24:10 +0300

    user controller intro

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,3 +1,43 @@
 'use strict'
 
-//! token model and controller first
+const User = require("../models/user");
+
+const Token = require("../models/token");
+
+const passwordEncrypt = require("../helpers/passwordEncrypt");
+
+module.exports = {
+
+    list: async(req, res) => {
+        const data = await User.find();
+        res.status(200).send({
+            error: false,
+            data
+        });
+    },
+
+    create: async(req, res) => {
+
+    },
+
+    read: async(req, res) => {
+
+    },
+
+    update: async(req, res) => {
+
+    },
+
+    delete: async(req, res) => {
+
+    },
+
+    login: async(req, res) => {
+
+    },
+
+    logout: async(req, res) => {
+
+    }
+
+};
