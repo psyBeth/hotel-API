d8cd6ff5a660538c91a23815969e478f11e60039
Betul <betulkoru06@gmail.com>
2024-05-12 20:04:13 +0300

    user controller crud

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -8,7 +8,7 @@ const passwordEncrypt = require("../helpers/passwordEncrypt");
 
 module.exports = {
 
-    list: async(req, res) => {
+    list: async (req, res) => {
         const data = await User.find();
         res.status(200).send({
             error: false,
@@ -16,27 +16,46 @@ module.exports = {
         });
     },
 
-    create: async(req, res) => {
+    create: async (req, res) => {
+        const data = await User.create(req.body);
+        res.status(201).send({
+            error: false,
+            body: req.body,
+            data: data,
+        });
+    },
+
+    read: async (req, res) => {
+        const data = await User.findOne({ _id: req.params.userId });
+        res.status(202).send({
+            error: false,
+            data: data,
+        });
+    },
+
+    update: async (req, res) => {
+        const data = await User.updateOne({ _id: req.params.userId }, req.body);
+        const newdata = await User.find({ _id: req.params.userId });
+        res.status(202).send({
+            error: false,
+            data: data,
+            newdata: newdata,
+        });
+    },
+
+    delete: async (req, res) => {
+        const data = await User.deleteOne({ _id: req.params.userId });
+        res.status(data.deletedCount >= 1 ? 204 : 404).send({
+            error: !data.deletedCount,
+            data
+        });
+    },
+
+    login: async (req, res) => {
 
     },
 
-    read: async(req, res) => {
-
-    },
-
-    update: async(req, res) => {
-
-    },
-
-    delete: async(req, res) => {
-
-    },
-
-    login: async(req, res) => {
-
-    },
-
-    logout: async(req, res) => {
+    logout: async (req, res) => {
 
     }
 
