adefcd6cf76cceedbff1ff57ed362b3596e9864c
Betul <betulkoru06@gmail.com>
2024-05-14 10:29:11 +0300

    user cntrlr login logout

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -52,11 +52,54 @@ module.exports = {
     },
 
     login: async (req, res) => {
+        const { username, email, password } = req.body;
 
+        if ((username || email) && password) {
+            const user = await User.findOne({ $or: [{ username }, { email }] });
+
+            if (user && user.password == passwordEncrypt(password)) {
+                if (user.isActive) {
+                    /* SIMPLE TOKEN */
+
+                    let tokenData = await Token.findOne({ userId: user.id });
+
+                    if (!tokenData)
+                        tokenData = await Token.create({
+                            userId: user.id,
+                            token: passwordEncrypt(user.id + Date.now())
+                        });
+
+                    /* SIMPLE TOKEN */
+
+                    res.status(200).send({
+                        error: false,
+                        token: tokenData.token,
+                        user
+                    });
+                } else {
+                    res.errorStatusCode = 401;
+                    throw new Error("This account is not active.");
+                };
+            } else {
+                res.errorStatusCode = 401;
+                throw new Error("Wrong username/email or password.");
+            };
+        } else {
+            res.errorStatusCode = 401;
+            throw new Error("Please enter username/email and password.");
+        };
     },
 
     logout: async (req, res) => {
+        const auth = req.headers?.authorization; // Token ...tokenKey...
+        const tokenKey = auth ? auth.split(" ") : null; // ['Token', '...tokenKey...']
+        const result = await Token.deleteOne({ token: tokenKey[1] });
 
+        res.send({
+            error: false,
+            message: "Token deleted. Logout was OK.",
+            result
+        });
     }
 
 };
