6983dffa663bc07eca69d430cf87f5c5d823678b
Betul <betulkoru06@gmail.com>
2024-05-15 17:47:24 +0300

    permission middleware

---
+++ src/middlewares/permissions.js
@@ -0,0 +1,24 @@
+'use strict'
+
+// AUTHORIZATION
+
+module.exports = {
+
+    isLogin: (req, res, next) => {
+
+        next();
+
+    },
+
+    isAdmin: (req, res, next) => {
+
+        next();
+
+        if (req.user && req.user.isActive && req.user.isAdmin) {
+            next();
+        } else {
+            res.errorStatusCode = 403;
+            throw new Error(' You must login and must be admin.');
+        };
+    }
+};
