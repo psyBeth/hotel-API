3b3ab314a336e615f188c6a46c70156ef3ef1d3e
Betul <betulkoru06@gmail.com>
2024-05-15 17:49:34 +0300

    reservation router done

--- src/routes/reservation.js
+++ src/routes/reservation.js
@@ -3,5 +3,18 @@
 const router = require("express").Router();
 
 const reservation = require("../controllers/reservation");
+const permissions = require("../middlewares/permissions");
 
 //? URL: /reservations
+
+router.route("/")
+    .get(reservation.list)
+    .post(reservation.create);
+
+router.route("/:id")
+    .get(permissions.isLogin, reservation.read)
+    .put(permissions.isAdmin, reservation.update)
+    .patch(permissions.isAdmin, reservation.update)
+    .delete(permissions.isAdmin, reservation.delete);
+
+module.exports = router;
